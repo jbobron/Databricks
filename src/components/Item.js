4cc26cf2bdded3308d7a1771ad902bf69efc4e79
Jake Obron <jbobron@gmail.com>
2015-08-28 19:54:15 -0700

    sample testing using mocha working, need to eventually replace with real testing

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,6 +1,5 @@
 var React = require('react');
 
-
 //create path param/attribute that is appended to each time we recursivly render Item
 //so we can pass "path" as the param to collapse function
 var Item = React.createClass({
@@ -8,23 +7,31 @@ var Item = React.createClass({
     if(this.props.tree.children){
       //need to make a reference to "this" context bc of new function scope in map
       var self = this;
+      
       var listItems = this.props.tree.children.map(function(node){
-        //bind collapse func b/c 'this' inside map is the window, we need
-        //collapse bound to the react component
-        // if(node.name === 'project1') debugger;
+        var path = self.props.path || [];
         debugger;
-        var boundClick = self.props.collapse.bind(self, node.name);
-        return <Item 
-                myCollapse={boundClick} 
+        console.log(node.name)
+        //pathSoFar.push(node.name)
+        path.push(node.name)
+        return <Item
+                path = {path}
                 collapse={self.props.collapse}
                 isCollapsed={node.isCollapsed} 
                 tree={node} 
                 name={node.name}/>
       })
-    }    
+      //pathSoFar.pop()
+    }
+    // if(pathSoFar.length === ){
+    //   path.push(pathSoFar.slice())
+    // }
+      
+    //bind collapse func b/c we need collapse bound to the react component 
+    
     return(
       <div>
-        <li onClick={this.props.collapse.bind(this,this.props.tree.name)}>{this.props.tree.name}</li>
+        <li path={this.props.path} onClick={this.props.collapse.bind(this, this.props.tree.name)}>{this.props.tree.name}</li>
         <ul style={this.props.tree.isCollapsed ? {display: 'none'} : {}}>
           {listItems}
         </ul>
